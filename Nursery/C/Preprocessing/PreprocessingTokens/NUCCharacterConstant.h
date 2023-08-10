4a15f6cc1406b825737fc0eff2a9ef1946dc6b06
TAKATA Akifumi <akifumi@mac.com>
2023-08-10 12:25:37 +0900

    Continue to implement C preprocessor

---
+++ Nursery/C/Preprocessing/PreprocessingTokens/NUCCharacterConstant.h
@@ -0,0 +1,17 @@
+//
+//  NUCCharacterConstant.h
+//  Nursery
+//
+//  Created by TAKATA Akifumi on 2023/08/10.
+//  Copyright © 2023 com.lily-bud. All rights reserved.
+//
+
+#import "NUCDecomposedPreprocessingToken.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NUCCharacterConstant : NUCDecomposedPreprocessingToken
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Nursery/C/Preprocessing/PreprocessingTokens/NUCCharacterConstant.m
@@ -0,0 +1,13 @@
+//
+//  NUCCharacterConstant.m
+//  Nursery
+//
+//  Created by TAKATA Akifumi on 2023/08/10.
+//  Copyright © 2023 com.lily-bud. All rights reserved.
+//
+
+#import "NUCCharacterConstant.h"
+
+@implementation NUCCharacterConstant
+
+@end
