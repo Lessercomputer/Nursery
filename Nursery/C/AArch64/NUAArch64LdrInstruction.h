0496e0f5bc40205419921b7462251c001b3d68c2
akiha <akiha_a@icloud.com>
2025-12-07 13:36:27 +0900

    Continue to implement C compiler. Mach-O

---
+++ Nursery/C/AArch64/NUAArch64LdrInstruction.h
@@ -0,0 +1,17 @@
+//
+//  NUAArch64LdrInstruction.h
+//  Nursery
+//
+//  Created by akiha on 2025/10/29.
+//  Copyright © 2025 com.lily-bud. All rights reserved.
+//
+
+#import "NUAArch64Instruction.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NUAArch64LdrInstruction : NUAArch64Instruction
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Nursery/C/AArch64/NUAArch64LdrInstruction.m
@@ -0,0 +1,13 @@
+//
+//  NUAArch64LdrInstruction.m
+//  Nursery
+//
+//  Created by akiha on 2025/10/29.
+//  Copyright © 2025 com.lily-bud. All rights reserved.
+//
+
+#import "NUAArch64LdrInstruction.h"
+
+@implementation NUAArch64LdrInstruction
+
+@end
